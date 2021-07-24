da2634acdb39e9057a437bc6218a821802ecd144
denise_taiza <maria_alyssa_taiza@dlsu.edu.ph>
2021-07-24 21:07:40 +0800

    Clear balls added

--- Assets/Scripts/Broadcasting/EventNames.cs
+++ Assets/Scripts/Broadcasting/EventNames.cs
@@ -6,4 +6,5 @@ public class EventNames
     // Start is called before the first frame update
 
     public const string ON_SPAWN_BUTTON_CLICKED = "ON_SPAWN_BUTTON_CLICKED";
+    public const string ON_CLEAR_BUTTON_CLICKED = "ON_CLEAR_BUTTON_CLICKED";
 }
